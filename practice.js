7784f9ce8eb9756b04b1dcb8820ece4b63ae9cb6
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-07-19 23:06:21 -0400

    " Arrays practice "

---
+++ practice.js
@@ -0,0 +1,18 @@
+function newBmxBikes(){
+    let arrayOfBikes = ['s&m', 'stranger', 'fit'];
+    const whichBike = arrayOfBikes[1];
+    console.log(whichBike);
+    
+    let newBike = 'skavenger'
+    arrayOfBikes.push(newBike);
+    console.log(arrayOfBikes);
+    
+    howMany = arrayOfBikes.length;
+    console.log(howMany);
+    
+    slicing = arrayOfBikes.slice(0,1);
+    console.log(slicing);
+    };
+    newBmxBikes();
+    
+    
